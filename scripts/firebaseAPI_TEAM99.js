bcf878c8ea7af1b447300b2d07e3fb411f64bec0
Bdiep6 <bdiep6@my.bcit.ca>
2024-02-20 10:55:07 -0800

    Added html, JS and Firebase Authentication

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,14 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyA8IGfk5Vg63Dr-nQiw4m22ALnlKn_hVV8",
+    authDomain: "comp1800demo-2d3ee.firebaseapp.com",
+    projectId: "comp1800demo-2d3ee",
+    storageBucket: "comp1800demo-2d3ee.appspot.com",
+    messagingSenderId: "69191231601",
+    appId: "1:69191231601:web:125a2d3ed535cc8f9613d0"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
